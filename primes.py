e1b5c9f68a0efc2f14d16f9ea874520d040a77e8
Zehra Hasan <zehrahasan157@hotmail.com>
2023-09-30 19:21:52 +0100

    add function to check if number is prime

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkPrime(num):
+    if num < 2:
+        return False
+    for i in range (2, num):
+        if num % i == 0:
+            return False
+    return True
+
 def primes(number_of_primes):
     list = []
+    currentNum = 0
+    while (len(list) < number_of_primes):
+        if checkPrime(currentNum):
+            list.append(currentNum)
+        currentNum += 1
     return list
